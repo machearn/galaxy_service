0a5edb1484b7665fa7914c892a4f91023d032393
machearn <machearn.n@gmail.com>
2023-04-18 15:39:53 -0400

    feat(config): use viper manage and load config

---
+++ util/config.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config is a struct that holds the configuration for the application.
+type Config struct {
+	// Database holds the database configuration.
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(configPath string) (Config, error) {
+	var config Config
+	v := viper.New()
+	v.AddConfigPath(configPath)
+	v.SetConfigName("app")
+	v.SetConfigType("env")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return config, err
+	}
+
+	if err := v.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
