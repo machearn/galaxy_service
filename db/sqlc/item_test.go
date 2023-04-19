ac49bd8bbd9ec168d0947145d55bee1f92bc6e18
machearn <machearn.n@gmail.com>
2023-04-18 23:29:35 -0400

    refactor(format): reformat `item_test.go`

--- db/sqlc/item_test.go
+++ db/sqlc/item_test.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"testing"
 
-	util "github.com/machearn/galaxy_service/util"
+	"github.com/machearn/galaxy_service/util"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,122 +16,119 @@ func CreateRandomItem(t *testing.T) Item {
 		Price:  int32(util.GetRandomInt(1000, 10000000)),
 	}
 
-  item, err := testQueries.CreateItem(context.Background(), arg)
+	item, err := testQueries.CreateItem(context.Background(), arg)
 
-  require.NoError(t, err)
-  require.NotEmpty(t, item)
+	require.NoError(t, err)
+	require.NotEmpty(t, item)
 
-  require.Equal(t, arg.Name, item.Name)
-  require.Equal(t, arg.Amount, item.Amount)
-  require.Equal(t, arg.Price, item.Price)
-  
-  require.NotZero(t, item.ID)
+	require.Equal(t, arg.Name, item.Name)
+	require.Equal(t, arg.Amount, item.Amount)
+	require.Equal(t, arg.Price, item.Price)
 
-  return item
+	require.NotZero(t, item.ID)
+
+	return item
 }
 
 func TestCreateItem(t *testing.T) {
-  CreateRandomItem(t)
+	CreateRandomItem(t)
 }
 
 func TestGetItem(t *testing.T) {
-  item := CreateRandomItem(t)
+	item := CreateRandomItem(t)
 
-  item2, err := testQueries.GetItem(context.Background(), item.ID)
+	item2, err := testQueries.GetItem(context.Background(), item.ID)
 
-  require.NoError(t, err)
-  require.NotEmpty(t, item2)
+	require.NoError(t, err)
+	require.NotEmpty(t, item2)
 
-  require.Equal(t, item.Name, item2.Name)
-  require.Equal(t, item.Amount, item2.Amount)
-  require.Equal(t, item.Price, item2.Price)
+	require.Equal(t, item.Name, item2.Name)
+	require.Equal(t, item.Amount, item2.Amount)
+	require.Equal(t, item.Price, item2.Price)
 
-  require.NotZero(t, item2.ID)
+	require.NotZero(t, item2.ID)
 }
 
 func TestListItems(t *testing.T) {
-  var items []Item
-  for i := 0; i < 10; i++ {
-    item := CreateRandomItem(t)
+	var items []Item
+	for i := 0; i < 10; i++ {
+		item := CreateRandomItem(t)
 
-    items = append(items, item)
-  }
+		items = append(items, item)
+	}
 
-  arg := ListItemsParams{
-    Limit: 5,
-    Offset: 2,
-  }
+	arg := ListItemsParams{
+		Limit:  5,
+		Offset: 2,
+	}
 
-  items2, err := testQueries.ListItems(context.Background(), arg)
+	items2, err := testQueries.ListItems(context.Background(), arg)
 
+	require.NoError(t, err)
+	require.NotEmpty(t, items2)
 
+	require.Len(t, items2, 5)
 
-  require.NoError(t, err)
-  require.NotEmpty(t, items2)
+	for index, item := range items2 {
+		require.NotEmpty(t, item)
 
-  require.Len(t, items2, 5)
+		require.Equal(t, items[index].Name, item.Name)
+		require.Equal(t, items[index].Amount, item.Amount)
+		require.Equal(t, items[index].Price, item.Price)
 
-  for index, item := range items2 {
-    require.NotEmpty(t, item)
-
-    require.Equal(t, items[index].Name, item.Name)
-    require.Equal(t, items[index].Amount, item.Amount)
-    require.Equal(t, items[index].Price, item.Price)
-
-    require.NotZero(t, item.ID)
-  }
+		require.NotZero(t, item.ID)
+	}
 }
 
 func TestUpdateItem(t *testing.T) {
-  item := CreateRandomItem(t)
+	item := CreateRandomItem(t)
 
-  arg := UpdateItemParams{
-    ID: item.ID,
-    Name: sql.NullString{String: util.GetRandomString(5), Valid: true},
-    Amount: sql.NullInt32{Int32: int32(util.GetRandomInt(10, 1000)), Valid: true},
-    Price: sql.NullInt32{Int32: int32(util.GetRandomInt(1000, 10000000)), Valid: true},
-  }
+	arg := UpdateItemParams{
+		ID:     item.ID,
+		Name:   sql.NullString{String: util.GetRandomString(5), Valid: true},
+		Amount: sql.NullInt32{Int32: int32(util.GetRandomInt(10, 1000)), Valid: true},
+		Price:  sql.NullInt32{Int32: int32(util.GetRandomInt(1000, 10000000)), Valid: true},
+	}
 
-  item2, err := testQueries.UpdateItem(context.Background(), arg)
+	item2, err := testQueries.UpdateItem(context.Background(), arg)
 
-  require.NoError(t, err)
-  require.NotEmpty(t, item2)
+	require.NoError(t, err)
+	require.NotEmpty(t, item2)
 
-  require.Equal(t, arg.Name.String, item2.Name)
-  require.Equal(t, arg.Amount.Int32, item2.Amount)
-  require.Equal(t, arg.Price.Int32, item2.Price)
+	require.Equal(t, arg.Name.String, item2.Name)
+	require.Equal(t, arg.Amount.Int32, item2.Amount)
+	require.Equal(t, arg.Price.Int32, item2.Price)
 
-  require.NotZero(t, item2.ID)
+	require.NotZero(t, item2.ID)
 }
 
 func TestPatialUpdateItem(t *testing.T) {
-  item := CreateRandomItem(t)
+	item := CreateRandomItem(t)
 
-  arg := UpdateItemParams {
-    ID: item.ID,
-    Amount: sql.NullInt32{Int32: int32(util.GetRandomInt(10, 1000)), Valid: true},
-  }
+	arg := UpdateItemParams{
+		ID:     item.ID,
+		Amount: sql.NullInt32{Int32: int32(util.GetRandomInt(10, 1000)), Valid: true},
+	}
 
-  item2, err := testQueries.UpdateItem(context.Background(), arg)
+	item2, err := testQueries.UpdateItem(context.Background(), arg)
 
-  require.NoError(t, err)
-  require.NotEmpty(t, item2)
+	require.NoError(t, err)
+	require.NotEmpty(t, item2)
 
-  require.Equal(t, arg.Amount.Int32, item2.Amount)
+	require.Equal(t, arg.Amount.Int32, item2.Amount)
 
-  require.NotZero(t, item2.ID)
+	require.NotZero(t, item2.ID)
 }
 
 func TestDeleteItem(t *testing.T) {
-  item := CreateRandomItem(t)
+	item := CreateRandomItem(t)
 
-  err := testQueries.DeleteItem(context.Background(), item.ID)
+	err := testQueries.DeleteItem(context.Background(), item.ID)
 
-  require.NoError(t, err)
+	require.NoError(t, err)
 
-  item2, err := testQueries.GetItem(context.Background(), item.ID)
+	item2, err := testQueries.GetItem(context.Background(), item.ID)
 
-  require.Error(t, err)
-  require.Empty(t, item2)
+	require.Error(t, err)
+	require.Empty(t, item2)
 }
-
