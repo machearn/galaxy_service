9618f608c39cade13892e3ffb1d919302c4b41a9
Pengtao <machearn.n@gmail.com>
2023-04-20 21:51:50 -0400

    feat(database): add transaction wrapper function and one transaction (#10)

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	config, err := util.LoadConfig("../..")
@@ -21,13 +22,13 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
-	dbConn, err := sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("failed to connect to db:", err)
 		os.Exit(1)
 	}
 
-	testQueries = New(dbConn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
