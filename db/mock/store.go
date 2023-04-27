734d085aa3e6f7a632cd67d94a6654f7072b47d0
Pengtao <machearn.n@gmail.com>
2023-04-26 23:30:32 -0400

    feat(tx): implement `CreateMember` transaction (#19)

--- db/mock/store.go
+++ db/mock/store.go
@@ -80,6 +80,21 @@ func (mr *MockStoreMockRecorder) CreateMember(arg0, arg1 interface{}) *gomock.Ca
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateMember", reflect.TypeOf((*MockStore)(nil).CreateMember), arg0, arg1)
 }
 
+// CreateMemberTx mocks base method.
+func (m *MockStore) CreateMemberTx(arg0 context.Context, arg1 db.CreateMemberTxParams, arg2 db.CreateSessionTxParams) (*db.CreateMemberResult, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateMemberTx", arg0, arg1, arg2)
+	ret0, _ := ret[0].(*db.CreateMemberResult)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CreateMemberTx indicates an expected call of CreateMemberTx.
+func (mr *MockStoreMockRecorder) CreateMemberTx(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateMemberTx", reflect.TypeOf((*MockStore)(nil).CreateMemberTx), arg0, arg1, arg2)
+}
+
 // CreateSession mocks base method.
 func (m *MockStore) CreateSession(arg0 context.Context, arg1 db.CreateSessionParams) (db.Session, error) {
 	m.ctrl.T.Helper()
