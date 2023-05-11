e33e77f58d43a03a176be987902ba8d0e6230b4c
machearn <machearn.n@gmail.com>
2023-05-11 11:35:31 -0400

    fix(migrate): import file source driver for migrate

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/hibiken/asynq"
 	_ "github.com/lib/pq"
 	"github.com/machearn/galaxy_service/api"
