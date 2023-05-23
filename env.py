4e887b4126211699d16a94d68b92812b6bad8e62
machearn <machearn.n@gmail.com>
2023-05-22 23:48:44 -0400

    feat(ci): retrieve secrets from aws

---
+++ env.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f"failed to get the {var_name} environment variable"
+        raise Exception(error_msg)
+
+def read_env_file(filename):
+    with open(filename) as f:
+        env = {}
+        for line in f:
+            line = line.strip()
+            if line.startswith('#') or '=' not in line:
+                continue
+            key, value = line.split('=', 1)
+            env[key] = value
+    return env
+
+def write_env_file(filename, env):
+    with open(filename, 'w') as f:
+        for key, value in env.items():
+            f.write(f"{key}={value}\n")
+
+def main():
+    env = read_env_file('app.env')
+    env['DB_SOURCE'] = get_env_variable('GALAXY_DB_SOURCE')
+    env['GRPC_SERVER_ADDRESS'] = get_env_variable('GALAXY_GRPC_SERVER_ADDRESS')
+    env['TOKEN_SYMMETRIC_KEY'] = get_env_variable('GALAXY_TOKEN_SYMMETRIC_KEY')
+    env['REDIS_ADDRESS'] = get_env_variable('GALAXY_REDIS_ADDRESS')
+    write_env_file('app.env', env)
+
+if __name__ == '__main__':
+    main()
