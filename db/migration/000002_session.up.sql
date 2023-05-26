cdc3617072e80def7ee636514cdb074365d2d549
machearn <machearn.n@gmail.com>
2023-05-25 23:42:30 -0400

    fix(database): remove unique constraint of session table

--- db/migration/000002_session.up.sql
+++ db/migration/000002_session.up.sql
@@ -2,8 +2,8 @@ ALTER TABLE "members" ADD COLUMN "password" varchar NOT NULL;
 
 CREATE TABLE "sessions" (
   "id" uuid PRIMARY KEY,
-  "member_id" integer UNIQUE NOT NULL,
-  "refresh_token" varchar UNIQUE NOT NULL,
+  "member_id" integer NOT NULL,
+  "refresh_token" varchar NOT NULL,
   "client_ip" varchar NOT NULL,
   "user_agent" varchar NOT NULL,
   "is_blocked" boolean NOT NULL DEFAULT false,
